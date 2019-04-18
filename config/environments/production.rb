20d45042dbbe91be392901f691a79f0c1db7df5d
shayladev <shaydev@gmail.com>
2019-04-18 11:40:11 -0400

    devise for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'catsareevil.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'shayladevonish.herokuapp.com' }
 end
